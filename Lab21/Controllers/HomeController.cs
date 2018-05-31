281ff5effd7b6a5e62e7186cbb0a1140db6c0d29
Lovejoy-gitsit <26442317+Lovejoy-gitsit@users.noreply.github.com>
2018-05-31 16:13:04 -0400

    first commit

---
+++ Lab21/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab21.Models;
+
+namespace Lab21.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UserRegistration()
+        {
+            string[] UserDetails = { "First Name", "Last Name", "Email", "Phone Number", "Password"};
+            ViewBag.UserDetails = User;
+            return View();
+        }
+
+        public ActionResult Add(user newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+
+                ORM.users.Add(newUser);
+                ORM.SaveChanges();
+
+                ViewBag.Message = "Confirmed";
+                return View("Registration");
+                
+            }
+            else
+            {
+                ViewBag.Message = "There was an error";
+                return View("Error");
+            }
+
+        }
+
+        public ActionResult ItemList()
+        {
+            CoffeeShopDBEntities ORM = new CoffeeShopDBEntities();
+            
+            ViewBag.ItemsList = ORM.items.ToList();
+            return View();
+        }
+
+       
+
+        
+        
+
+    }
+}
---
+++ Lab21/Models/UserInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lab21.Models
+{
+    public class UserInfo
+    {
+        [Required]
+        public string FirstName { set; get; }
+
+        [Required]
+        public string LastName { set; get; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage = "Bad Email!")]
+        public string Email { set; get; }
+
+        [Required]
+        public string Phone { set; get; }
+
+
+        [Required]
+        public string Password { set; get; }
+
+        public UserInfo()
+        {
+
+        }
+
+        public UserInfo(string firstname, string lastname, string email, string phone, string password)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Email = email;
+            Phone = phone;
+            Password = password;
+        }
+
+
+    }
+}
